b1844ca6389af7e113b10db3a60a3f7a7f4fa69a
VeeOliver <98466281+VeeOliver@users.noreply.github.com>
2023-04-06 00:04:29 +0200

    add Id field to album

---
+++ models/albumModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+// create schema for album with id, title, artist name and year
+const albumSchema = mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: [true, "Please give this album an id number"]
+    },
+    title: {
+      type: String,
+      required: [true, "Please enter the title of the album"]
+    },
+    artist: {
+      type: String,
+      required: [true, "Please enter the name of the artist"]
+    },
+    year: {
+      type: Number,
+      required: [true, "Please enter the album year"]
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Album = mongoose.model('Album', albumSchema)
+
+module.exports = Album
